165647655c20c738a32535e6f854d920a7c2cc8c
Grant Chen <epicNecrovalley@gmail.com>
2016-07-27 11:00:26 -0700

    asd

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,7 +1,8 @@
 //set up the instance of the engine on the page
 var Q = Quintus()
-	.include("Sprites, Scenes, Input")
-	.setup();
+	.include("Sprites, Scenes, Input") //what quintus is using
+	.setup({width: 800, height: 600, ScaleToFit: true}); //the dimensions of the page are 800 pixels wide, 600 pixels high, and they fit the users device
+
 
 
 
