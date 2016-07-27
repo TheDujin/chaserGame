2c3ee30796f7a13bae69b72ff3f9f157bd93dede
TheDujin <kevindujin@gmail.com>
2016-07-27 11:15:48 -0700

    Changed victim.js and commented out a bunch of shit in gameboard.js

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,8 +1,21 @@
 //set up the instance of the engine on the page
-var Q = Quintus()
-	.include("Sprites, Scenes, Input") //what quintus is using
-	.setup({width: 800, height: 600, ScaleToFit: true}); //the dimensions of the page are 800 pixels wide, 600 pixels high, and they fit the users device
+//var Q = Quintus()
+//	.include("Sprites, Scenes, Input") //what quintus is using
+//	.setup({width: 800, height: 600, ScaleToFit: true}); //the dimensions of the page are 800 pixels wide, 600 pixels high, and they fit the users device
 
+//some method that deals with a new player entering the game
+//calls var newPlayer 1 = new victim('some ID');
+
+
+
+
+
+
+
+
+var players = new Array();
+players[0] = new victim(1234);
+console.log(players[0].ID);
 
 
 
--- src/victim.js
+++ src/victim.js
@@ -5,15 +5,11 @@ var direction;
 var x;
 var y;
 
-
 function victim(ID){
 	this.ID = ID;
 	this.health = 100;
 	this.speed = 100;
 	this.direction = 0;
-	this.x = Math.randomInt(9999);
-	this.y = Math.randomInt(9999);
+	this.x = Math.floor(Math.random() * 9999);
+	this.y = Math.floor(Math.random() * 9999);
 }
-
-
-
